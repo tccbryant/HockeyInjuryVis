50188324cfd1dfba051974cb014ceb62e171645b
mshehan <mshehan@my.smccd.edu>
2018-06-09 19:50:20 -0700

    got tooltip up partially working. just need to hide it when not in use

--- Final Project/js/hockey_points.js
+++ Final Project/js/hockey_points.js
@@ -1,246 +1,115 @@
 var hockey_player = (function(){
-
-var annotations = [{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'head' },
-  y: 12,
-  x: 70,
-    dx: 5,
-  dy: 300,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'upperBody' },
-  y: 85,
-  x: 215,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'lowerBody' },
-  y: 243,
-  x: 272,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'leg' },
-  y: 312,
-  x: 189,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'foot' },
-  y: 386,
-  x: 170,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'hand' },
-  y: 151,
-  x: 255,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'groin' },
-  y: 199,
-  x: 230,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'shoulder' },
-  y: 33,
-  x: 210,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'back' },
-  y: 111,
-  x: 241,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'face' },
-  y: 49,
-  x: 152,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'neck' },
-  y: 78,
-  x: 166,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'torso' },
-  y: 144,
-  x: 205,
-    dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-},
-{
-  note: {
-    label: "Longer text to show text wrapping",
-    title: "Annotations :)"
-  },
-  //can use x, y directly instead of data
-  data: { injury: 'arm' },
-  y: 142,
-  x: 149,
-  dx: 78,
-  dy: 380,
-  subject: {
-    radius: 5,
-    radiusPadding: 5
-  }
-}
+var csv = 'data/AggregateInjuries(1).csv';  
+var svg;
+var body_locations = [
+    {type: 'Head', left: 119, top: 18},
+    {type: 'Upper body', left: 215, top: 85},
+    {type: 'Lower body', left: 272, top: 243},
+    {type: 'Leg', left: 189, top: 312},
+    {type: 'Foot', left: 170, top: 386},
+    {type: 'Hand', left: 255, top: 151},
+    {type: 'Groin', left: 230, top: 199},
+    {type: 'Shoulder', left: 210, top: 33},
+    {type: 'Back', left: 241, top: 111},
+    {type: 'Face', left: 139, top: 50},
+    {type: 'Neck', left: 166, top: 78},
+    {type: 'Torso', left: 205, top: 144},
+    {type: 'Arm', left: 149, top: 142}
 ];
-//var body_locations = [
-//    {type: 'head', left: 133, top: 12},
-//    {type: 'upperBody', left: 215, top: 85},
-//    {type: 'lowerBody', left: 272, top: 243},
-//    {type: 'leg', left: 189, top: 312},
-//    {type: 'foot', left: 170, top: 386},
-//    {type: 'hand', left: 255, top: 151},
-//    {type: 'groin', left: 230, top: 199},
-//    {type: 'shoulder', left: 210, top: 33},
-//    {type: 'back', left: 241, top: 111},
-//    {type: 'face', left: 152, top: 49},
-//    {type: 'neck', left: 166, top: 78},
-//    {type: 'torso', left: 205, top: 144},
-//    {type: 'arm', left: 149, top: 142}
-//];
 
+function getInjuryData(csv){
+    return new Promise(function(resolve,reject){
+        d3.csv(csv, function(error, data){
+            if(error){
+                reject(error);
+            }
+            resolve(data); 
+        });
+    }); 
+}
+function mergeCSVandPoints(data){   
+    
+    data.forEach(function(point){
+        var found = body_locations.find(function(element){
+            return element.type === point.injury_type;
+        });
+        if(found) {
+            point['left'] = found.left,
+            point['top'] = found.top;
+        }
+    });
+    return data;
+}
 function printXY(){
     d3.select('.hockey-player-svg-elem').on('click', function(){
        var top_off = d3.select(this).node().getBoundingClientRect().top,
            left_off = d3.select(this).node().getBoundingClientRect().left;
         
-        console.log("left: " + (d3.event.clientX - left_off) + ", top: " + (d3.event.clientY - top_off));
-        
+        console.log("left: " + (d3.event.offsetX - 10) + ", top: " + (d3.event.offsetY - 20));
+      console.log(d3.event);
     });
 }
 
+function drawToolTipDiv(){
+    d3.select("body")
+        .select(".hockey-player")
+        .append("div")
+        .attr('class', 'tool-tip');
+}
+function bindOnClickPoints(data){
+    
+    d3.selectAll('.body-point')
+        .on('click', function(){
+            
+            var classes = d3.select(this).attr('class').split(/\s/);
+            var body_part = classes.length > 2 ? classes[1] + " " + classes[2] : classes[1];
+            
+            var found = data.find(function(element){
+                return body_part === element.injury_type;
+            });
+            
+            if(found){
+                var golie_injuries = (found.injuries_g/found.num_injuries)*100,
+                    defense_injuries = (found.injuries_d/found.num_injuries)*100,
+                    forward_injuries = (found.injuries_f/found.num_injuries)*100;
+                
+                d3.select('.injury-type').text(body_part);
+                
+                d3.select('.severity').text((found.total_severity / found.num_injuries).toFixed(2));
+                
+                d3.select('.inj-list').text(found.notes);
+                
+                d3.select(".golie-inj-percentage").text(golie_injuries.toFixed(2));
+                
+                d3.select(".defense-inj-percentage").text(defense_injuries.toFixed(2));
+                
+                d3.select(".forward-inj-percentage").text(forward_injuries.toFixed(2));
+            }
+        })
+}
+function drawCircles(data){
+    svg
+        .append('g')
+        .attr('class', 'body-points-group')
+        .selectAll('circle')
+        .data(data)
+        .enter()
+        .append('circle')
+        .attr('class', function(d){ return 'body-point' + " " + d.injury_type; })
+		.attr('cx', function (d) { return d.left; })
+		.attr('cy', function (d) { return d.top; })
+        .attr('r', function(d){return 7;})
+        .attr('stroke', 'red')
+        .style("pointer-events","visible")
+        .attr('fill', 'none')
+    
+}
 function drawBodyPoints(){
     var margin = {left: 10, right: 10, bottom: 20, top: 20};
     var width = 320 - (margin.left + margin.right);
     var height = 443 - (margin.top + margin.bottom);
     var img_width = 300, img_height = 423; 
-    var svg = d3.select('.hockey-player')
+    svg = d3.select('.hockey-player')
         .append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
@@ -255,33 +124,12 @@ function drawBodyPoints(){
                 .attr("width", img_width)
                 .attr("height", img_height);
     
-    var type = d3.annotationCalloutCircle;
-    var makeAnnotations = d3.annotation()
-          .editMode(true)
-          .type(type)
-          //accessors & accessorsInverse not needed
-          //if using x, y in annotations JSON
-          
-          .annotations(annotations)
-    svg
-      .append("g")
-      .attr("class", "annotation-group")
-      .call(makeAnnotations)
-//    d3.select('.hockey-player')
-//        .append('g')
-//        .attr('class', 'body-points-group')
-//        .selectAll('div')
-//        .data(body_locations)
-//        .enter()
-//        .append('div')
-//        .text(function(d){ return d.type;})
-//        .attr('class', 'body-point')
-//        .style('position','absolute')
-//		.style('top', function (d) { return d.top + 'px'; })
-//		.style('left', function (d) { return d.left + 'px'; })
-//        .style('width', '10px')
-//        .style('height', '10px')
-//        .style('background-color', 'red')  
+    getInjuryData(csv).then(mergeCSVandPoints).then(function(data){
+        drawCircles(data);
+        bindOnClickPoints(data);
+    })
+    
+     
     
 }
 
@@ -291,4 +139,4 @@ return {
 }
 })()
 hockey_player.draw();
-hockey_player.printXY();
+
